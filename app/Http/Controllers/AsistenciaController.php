598bba9658a3cb6b1817e23d47d1c6e276119569
OswaldoHB55 <nestor.72607424@gmail.com>
2024-07-02 14:39:22 -0600

    cambio innesesario

--- app/Http/Controllers/AsistenciaController.php
+++ app/Http/Controllers/AsistenciaController.php
@@ -59,7 +59,7 @@ class AsistenciaController extends Controller
 
     public function edit($id)
     {
-        $asistencia = Asistencia::find($id);
+        $asistencia = Asistencia::with('estudiante', 'grupo')->find($id);
 
         if (!$asistencia) {
             return abort(404);
