6387e5752df0d211e41f48b1220f2a159c219dd2
OswaldoHB55 <nestor.72607424@gmail.com>
2024-07-08 11:25:18 -0600

    mensaje

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,13 @@
 @extends('layouts.app')
 @section('content')
-    <div class="alert alert-success" role="alert">
-        Bienvenido a mi sitio web.........
-        para comenzar porfavor inicia sesion😊
+    <div class="card bg-info text-white" style="width: 18rem;">
+        <div class="card-body" style="color: black;">
+            @if (auth()->check())
+                <h5 class="card-title">Bienvenido, {{ auth()->user()->nombre }}! 😊</h5>
+            @else
+                <h5 class="card-title">Bienvenido a mi sitio web</h5>
+                <p class="card-text">Para comenzar, por favor inicia sesión 😊</p>
+            @endif
+        </div>
     </div>
 @endsection
