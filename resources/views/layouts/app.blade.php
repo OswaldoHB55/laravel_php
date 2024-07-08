a2fa090933c632f9e8428642f8a92d3e48466151
OswaldoHB55 <nestor.72607424@gmail.com>
2024-07-08 11:02:32 -0600

    simples

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -73,8 +73,8 @@
                         </div>
                     </div>
                     <div class="sb-sidenav-footer">
-                        <div class="small">Logged in as:</div>
-                        Start Bootstrap
+                        <div class="small">Login:</div>
+                        PHP Laravel 2024
                     </div>
                 </nav>
             </div>
@@ -87,7 +87,7 @@
                 <footer class="py-4 bg-light mt-auto">
                     <div class="container-fluid px-4">
                         <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; Your Website 2023</div>
+                            <div class="text-muted">ESFE Agape 2024</div>
                             <div>
                                 <a href="#">Privacy Policy</a>
                                 &middot;
